466d3847b333fc0b9cfde7a439072ec2a49affd0
Skibot <donaldhurd@gmail.com>
2015-04-27 10:13:29 -0600

    Committed by Udacity

---
+++ Pitch-Perfect/Pitch-Perfect/RecordedAudio.swift
@@ -0,0 +1,19 @@
+//
+//  RecordedAudio.swift
+//  Pitch-Perfect
+//
+//  Created by Donald Hurd on 4/6/15.
+//  Copyright (c) 2015 DonHurdApps. All rights reserved.
+//
+
+import Foundation
+
+
+class RecordedAudio: NSObject {
+        var filePathUrl: NSURL!
+        var title: String!
+    // init(url: NSURL!, title: String!) {
+    //    self.filePathUrl = url
+    //    self.title = title
+    //    }
+}
